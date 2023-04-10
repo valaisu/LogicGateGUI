dfdd3a2a9c7f2319573a5646708341f003b57a28
valaisu <63551725+valaisu@users.noreply.github.com>
2023-04-10 12:50:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LogicGateGUI
+ A software for designing small circuits out of logical gates
---
+++ main.py
@@ -0,0 +1,142 @@
+# This is going to be a program for building logical gates.
+# The idea is to make a graphical user interface. Take inspiration
+# for example from
+# https://www.youtube.com/watch?v=QZwneRb-zqA&t=194s
+
+#TODO: add draggable buttons
+#TODO: add ability to make connections between buttons
+#TODO: add connections also to the scene itself
+
+import pygame
+from typing import Type
+
+
+class inOut:
+    def __init__(self, isInput, id, connections):
+        self.isInput = isInput
+        self.id = id
+        self.connections = connections
+
+    def addConnection(self, connection): # connection consists of (buttonId, connectionId) pairs
+        self.connections.append(connection)
+
+class button:
+    def __init__(self, mid, width, height, color, colorHover, text, textColor, draggable):
+        self.mid = mid #coord 0,0 at upper left corner
+        self.width = width
+        self.height = height
+        self.left = mid[0]-width/2
+        self.right = mid[0]+width/2
+        self.top = mid[1]-height/2
+        self.bottom = mid[1]+height/2
+
+        self.color = color
+        self.colorHover = colorHover
+
+        self.text = smallfont.render(text, True, textColor)
+
+        self.draggable = draggable
+
+addedBtnCount = 0
+color = (255, 255, 255) # white
+color_light = (170, 170, 170)
+color_dark = (100, 100, 100)
+
+def add_button(btnlist, text):
+    btnlist.append(button((60 + addedBtnCount*100, 690), 80, 30, color_dark, color_light, text, (30, 30, 30), True))
+
+def updateBtn(mid, button):
+#def updateBtn(mid, button: Type[button]):
+    button.mid = mid
+    button.left = mid[0] - button.width / 2
+    button.right = mid[0] + button.width / 2
+    button.top = mid[1] - button.height / 2
+    button.bottom = mid[1] + button.height / 2
+
+
+pygame.init()
+res = (720, 720)
+
+# opens up a window
+screen = pygame.display.set_mode(res)
+
+width = screen.get_width()
+height = screen.get_height()
+
+# defining a font
+smallfont = pygame.font.SysFont('Corbel', 35)
+
+# rendering a text written in
+# this font
+text = smallfont.render('quit', True, color)
+
+
+quitButton = button([200, 200], 100, 30, color_dark, color_light, "quit", (30, 30, 30), False)
+addButton = button([200, 300], 100, 30, color_dark, color_light, "add", (30, 30, 30), False)
+
+buttons = [quitButton, addButton]
+
+
+print(quitButton.left, quitButton.right, quitButton.top, quitButton.bottom)
+
+dragging = False
+moveInfo = [(0,0),(0,0),-1]
+# = mouseCoords, buttonCoords, i
+
+while True:
+    mouse = pygame.mouse.get_pos()
+    for ev in pygame.event.get():
+
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+        # checks if a mouse is clicked
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+
+            # if the mouse is clicked on the
+            # button the game is terminated
+            # ADD FOR LOOP HERE LATER
+            for i in range(len(buttons)):
+                if buttons[i].left <= mouse[0] <= buttons[i].right and buttons[i].top <= mouse[1] <= buttons[i].bottom:
+                    if i == 0:
+                        pygame.quit()
+                    if i == 1:
+                        text = "test" + str(addedBtnCount)
+                        add_button(buttons, text)
+                        addedBtnCount += 1
+                    if buttons[i].draggable:
+                        dragging = True
+                        moveInfo = [mouse, buttons[i].mid, i]
+
+        if ev.type == pygame.MOUSEBUTTONUP:
+            if dragging:
+                dragging = False
+                updateBtn(mouse, buttons[moveInfo[2]])
+
+
+    screen.fill((60, 60, 60))
+
+
+    # if mouse is hovered on a button it
+    # changes to lighter shade
+    for i in range(len(buttons)):
+        if buttons[i].left <= mouse[0] <= buttons[i].right and buttons[i].top <= mouse[1] <= buttons[i].bottom:
+            if dragging and i == moveInfo[2]:
+                pygame.draw.rect(screen, buttons[i].colorHover, [mouse[0]-buttons[i].width/2, mouse[1]-buttons[i].height/2, buttons[i].width, buttons[i].height])
+            else:
+                pygame.draw.rect(screen, buttons[i].colorHover, [buttons[i].left, buttons[i].top, buttons[i].width, buttons[i].height])
+        else:
+            if dragging and i == moveInfo[2]:
+                pygame.draw.rect(screen, buttons[i].color, [mouse[0] - buttons[i].width / 2, mouse[1] - buttons[i].height / 2, buttons[i].width, buttons[i].height])
+            else:
+                pygame.draw.rect(screen, buttons[i].color, [buttons[i].left, buttons[i].top, buttons[i].width, buttons[i].height])
+
+    # add text on buttons
+    for i in range(len(buttons)):
+        if dragging and i == moveInfo[2]:
+            screen.blit(buttons[i].text, (mouse[0] - buttons[i].width / 2, mouse[1] - buttons[i].height / 2))
+        else:
+            screen.blit(buttons[i].text, (buttons[i].left, buttons[i].top))
+    pygame.draw.line(screen, (0,0,0), (50, 50), (200, 50))
+    # updates the frames of the game
+    pygame.display.update()
